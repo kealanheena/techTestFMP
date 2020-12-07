d5ecae79f92a4bcb9be94d2a10a36c6bcd3bff6f
kealanheena <kealanheena@gmail.com>
2020-12-07 03:19:23 +0000

    Feat: get prime numbers effectively for the first 10 prime numbers

--- __test__/getPrimeNumbers.test.js
+++ __test__/getPrimeNumbers.test.js
@@ -1,62 +1,72 @@
 const PrimeNumberMultiplicationTable = require('../lib/getPrimeNumbers');
 
 describe('#PrimeNumberMultiplicationTable', () => {
+
+  describe('#primeNumbers', () => {
+    test('prime numbers function should return a list of prime numbers', () => {
+      let testMultiplicationTable = new PrimeNumberMultiplicationTable(2)
+    
+      expect(testMultiplicationTable.primeNumbers).toBeInstanceOf(Function);
+    });
   
-  test('prime numbers function should return a list of prime numbers', () => {
-    let testMultiplicationTable = new PrimeNumberMultiplicationTable(2)
+    test('prime numbers function should return a list of prime numbers', () => {
+      let testMultiplicationTable = new PrimeNumberMultiplicationTable(1)
+    
+      expect(testMultiplicationTable.primeNumbers()).toBeInstanceOf(Array);
+    });
   
-    expect(testMultiplicationTable.primeNumbers).toBeInstanceOf(Function);
-  });
-
-  test('prime numbers function should return a list of prime numbers', () => {
-    let testMultiplicationTable = new PrimeNumberMultiplicationTable(1)
+    describe('prime numbers function should return', () => {
   
-    expect(testMultiplicationTable.primeNumbers()).toBeInstanceOf(Array);
-  });
+      test('the first 2 prime numbers when 2 is passed', () => {
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(2);
+      
+        expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3]);
+      });
+  
+      test('the first 3 prime numbers when 3 is passed', () => {
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(3);
+      
+        expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5]);
+      });
+  
+      test('the first 8 prime numbers when 8 is passed', () => {
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(10);
+      
+        expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+      });
 
-  describe('prime numbers function should return', () => {
-
-    test('the first 2 prime numbers when 2 is passed', () => {
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(2);
-    
-      expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3]);
     });
-
-    test('the first 2 prime numbers when 2 is passed', () => {
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(3);
     
-      expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5]);
+    describe('the length of the list of prime numbers should be equal to',() => {
+  
+      test('2 when 2 is passed as a variable', () => {
+        let numberOfPrimeNumbers = 2;
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+      
+        expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
+      });
+  
+      test('4 when 4 is passed as a variable', () => {
+        let numberOfPrimeNumbers = 4;
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+      
+        expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
+      });
+  
+      test('7 when 7 is passed as a variable', () => {
+        let numberOfPrimeNumbers = 7;
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+      
+        expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
+      });
+  
+      test('EDGE CASE: it should return "Error, Not a valid input" when 0 or less is passed as a variable', () => {
+        let numberOfPrimeNumbers = 0;
+        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+      
+        expect(testMultiplicationTable.primeNumbers()).toEqual("Error, Not a valid input");
+      });
     });
   });
   
-  describe('the length of the list of prime numbers should be equal to',() => {
-
-    test('2 when 2 is passed as a variable', () => {
-      let numberOfPrimeNumbers = 2;
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
-    
-      expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
-    });
-
-    test('4 when 4 is passed as a variable', () => {
-      let numberOfPrimeNumbers = 4;
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
-    
-      expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
-    });
-
-    test('7 when 7 is passed as a variable', () => {
-      let numberOfPrimeNumbers = 7;
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
-    
-      expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
-    });
-
-    test('EDGE CASE: it should return "Error, Not a valid input" when 0 or less is passed as a variable', () => {
-      let numberOfPrimeNumbers = 0;
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
-    
-      expect(testMultiplicationTable.primeNumbers()).toEqual("Error, Not a valid input");
-    });
-  });
 });
