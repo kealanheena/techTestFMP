d25604f2d67e0f770ac14b6b813f2ee4f7fe64b5
kealanheena <kealanheena@gmail.com>
2020-12-07 04:40:21 +0000

    Refactor: use beforeEach statement in tests to make code more DRY

--- __test__/PrimeNumberMultiplicationTable.test.js
+++ __test__/PrimeNumberMultiplicationTable.test.js
@@ -1,42 +1,49 @@
-const PrimeNumberMultiplicationTable = require('../lib/getPrimeNumbers');
+const PrimeNumberMultiplicationTable = require('../lib/PrimeNumberMultiplicationTable');
 
 describe('#PrimeNumberMultiplicationTable', () => {
 
+  let testMultiplicationTable, numberOfPrimeNumbers, numberOfPrimeNumbersUpdated;
+
+  beforeEach(() => {
+    numberOfPrimeNumbers = 1;
+    testMultiplicationTable = new PrimeNumberMultiplicationTable(1);
+  });
+
   describe('#primeNumbers', () => {
-    test('prime numbers function should return a list of prime numbers', () => {
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(2)
-    
+    test('prime numbers function should return a list of prime numbers', () => {  
       expect(testMultiplicationTable.primeNumbers).toBeInstanceOf(Function);
     });
   
     test('prime numbers function should return a list of prime numbers', () => {
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(1)
-    
       expect(testMultiplicationTable.primeNumbers()).toBeInstanceOf(Array);
     });
   
     describe('prime numbers function should return', () => {
   
       test('the first 2 prime numbers when 2 is passed', () => {
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(2);
+        numberOfPrimeNumbers = 2;
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3]);
       });
   
       test('the first 3 prime numbers when 3 is passed', () => {
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(3);
+        numberOfPrimeNumbers = 3;
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5]);
       });
   
       test('the first 10 prime numbers when 10 is passed', () => {
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(10);
+        numberOfPrimeNumbers = 10;
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
       });
 
       test('the first 16 prime numbers when 16 is passed', () => {
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(16);
+        numberOfPrimeNumbers = 16;
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers()).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]);
       });
@@ -46,29 +53,29 @@ describe('#PrimeNumberMultiplicationTable', () => {
     describe('the length of the list of prime numbers should be equal to',() => {
   
       test('2 when 2 is passed as a variable', () => {
-        let numberOfPrimeNumbers = 2;
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+        numberOfPrimeNumbers = 2;
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
       });
   
       test('4 when 4 is passed as a variable', () => {
         let numberOfPrimeNumbers = 4;
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
       });
   
       test('7 when 7 is passed as a variable', () => {
         let numberOfPrimeNumbers = 7;
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers().length).toEqual(numberOfPrimeNumbers);
       });
   
       test('EDGE CASE: it should return "Error, Not a valid input" when 0 or less is passed as a variable', () => {
         let numberOfPrimeNumbers = 0;
-        let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
+        testMultiplicationTable.updateAmount(numberOfPrimeNumbers);
       
         expect(testMultiplicationTable.primeNumbers()).toEqual("Error, Not a valid input");
       });
@@ -77,12 +84,10 @@ describe('#PrimeNumberMultiplicationTable', () => {
 
   describe('updateAmount', () => {
     test('when you update the amount it should return the new amount and change the amount', () => {
-      let numberOfPrimeNumbers = 0;
-      let updatedNumberOfPrimeNumbers = 0;
-      let testMultiplicationTable = new PrimeNumberMultiplicationTable(numberOfPrimeNumbers)
-    
-      expect(testMultiplicationTable.updateAmount(updatedNumberOfPrimeNumbers)).toEqual(updatedNumberOfPrimeNumbers);
-      expect(testMultiplicationTable.amount).toEqual(updatedNumberOfPrimeNumbers);
+      numberOfPrimeNumbersUpdated = 3;
+
+      expect(testMultiplicationTable.updateAmount(numberOfPrimeNumbersUpdated)).toEqual(numberOfPrimeNumbersUpdated);
+      expect(testMultiplicationTable.amount).toEqual(numberOfPrimeNumbersUpdated);
     });
   });
   
